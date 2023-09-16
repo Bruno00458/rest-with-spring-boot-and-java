368edc5ca2b8c98048d1ea38f482443e7d5507d0
Bruno Jesus <bruno00458@hotmail.com>
2023-09-16 18:16:36 -0300

    Trabalhando com exceções e criando uma calculadora

---
+++ 05_FirstStepInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/math/SimpleMath.java
@@ -0,0 +1,20 @@
+package br.com.math;
+
+public class SimpleMath {
+
+	public Double sum(Double numberOne, Double numberTwo) {
+		return numberOne + numberTwo;
+	}
+	
+	public Double division(Double numberOne, Double numberTwo) {
+		return numberOne / numberTwo;
+	}
+	
+	public Double mean(Double numberOne, Double numberTwo) {
+		return (numberOne + numberTwo) / 2;
+	}
+	
+	public Double squareRoot(Double number) {
+		return Math.sqrt(number);
+	}
+}
